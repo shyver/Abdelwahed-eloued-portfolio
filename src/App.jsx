e26e4950bde7260d4d891066160c6f99c667adad
unknown <eloued.abdelwahed@gmail.com>
2023-07-10 09:43:04 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { About , Contact , Experience , Feedbacks , Hero , Navbar , Tech , Works , StarsCanvas} from './components';
+
+const App = () => {
+
+  return (
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-coverbg-no-repeat bg center"> 
+        <Navbar/>
+        <Hero/>
+        </div>
+        <About/>
+        <Experience/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+        <div className = "relative z-0">
+          <Contact/>
+          <StarsCanvas/>
+        </div>
+      </div>
+  
+  )
+}
+
+export default App
